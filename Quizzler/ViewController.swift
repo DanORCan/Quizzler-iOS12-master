f9a599cc8b4e313e190218ddf169568225ca8c8d
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2018-11-17 14:27:55 -0500

    checkAnswer function ... print to console

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
+    let allQuestions = QuestionBank()
+    var pickedAnswer = false
     
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -21,10 +23,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+        
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
+        
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
   
     }
     
@@ -40,6 +53,14 @@ class ViewController: UIViewController {
     
     
     func checkAnswer() {
+        
+        let correctAnswer = allQuestions.list[0].answer
+        
+        if correctAnswer == pickedAnswer {
+            print("You got it!")
+        } else {
+            print("Wrong Answer!")
+        }
         
     }
     
